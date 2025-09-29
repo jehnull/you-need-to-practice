8fb15f52219ecd8308bf132fa9d1017a6cb16d2c
juhnull <madebyjehnull@gmail.com>
2025-09-29 13:02:20 -0700

    Update vite.config.js

--- yntp/vite.config.js
+++ yntp/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://jehnull.github.io/you-need-to-practice/yntp/',
   server: {
     port: 8080,
     open: true,
